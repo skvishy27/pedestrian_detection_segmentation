63876e83087b7633bb8c4e68d1f71231a3adabcf
skvis <vishalksingh97@gmail.com>
2020-06-21 21:39:59 +0530

    code added

---
+++ src/main.py
@@ -0,0 +1,149 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+import sys
+
+class PennFudanDataset(object):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        self.imgs = list(sorted(os.listdir(os.path.join(root, 'PNGImages'))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, 'PedMasks'))))
+
+    
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, 'PedMasks', self.masks[idx])
+        img = Image.open(img_path).convert('RGB')
+        mask = Image.open(mask_path)
+        mask = np.array(mask)
+        obj_ids = np.unique(mask)
+        obj_ids = obj_ids[1:]
+        masks = mask == obj_ids[:, None, None]
+        # print(masks[0][181][227])
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i]) # wherever True is present, it will return index of list and position of element
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target['boxes'] = boxes
+        target['labels'] = labels
+        target['masks'] = masks
+        target['image_id'] = image_id
+        target['area'] = area
+        target['iscrowd'] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+num_classes = 2
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+def get_model_instance_segmentation(num_classes):
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
+
+import transforms as T
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+
+from engine import train_one_epoch, evaluate
+import utils
+
+def main():
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    num_classes = 2
+    dataset = PennFudanDataset('../input/PennFudanPed', get_transform(train=True))
+    dataset_test = PennFudanDataset('../input/PennFudanPed', get_transform(train=False))
+
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    data_loader = torch.utils.data.DataLoader(dataset, 
+                                              batch_size=2,
+                                              shuffle=True,
+                                              num_workers=4,
+                                              collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(dataset_test,
+                                                   batch_size=1,
+                                                   shuffle=False,
+                                                   num_workers=4,
+                                                   collate_fn=utils.collate_fn)
+
+    model = get_model_instance_segmentation(num_classes)
+
+    model.to(device)
+
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params,
+                                lr=0.005,
+                                momentum=0.9,
+                                weight_decay=0.0005)
+
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        lr_scheduler.step()
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's it!")
+
+if __name__ == '__main__':
+    main()
